52a51440c542d4b644540e2deb70fab6cb78a2c6
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-02-23 16:05:38 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -115,7 +115,7 @@ void PipeCommand::execute() {
     int fdin;
     if (_inFile) {
       //open file
-      fdin = open(_inFile->c_str(), O_RDONLY, 0444);
+      fdin = open(_inFile->c_str(), O_RDONLY);
     } else {
       fdin = dup(tmpin);
     }
