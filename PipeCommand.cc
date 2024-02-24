21c59c5faf76b62f1872c51567f52d05dbf04922
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-02-24 16:46:55 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -108,7 +108,7 @@ void PipeCommand::execute() {
         Shell::TheShell->prompt();
         return;
     }
-    if (strcmp(_simpleCommands[0]->arguments[0],"exit") == 0) {
+    if (strcmp(_simpleCommands[0]->_arguments[0],"exit") == 0) {
       fprintf(stderr, "Good bye!!\n");
       exit(1);
     }
