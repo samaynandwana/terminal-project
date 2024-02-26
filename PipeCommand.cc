a138cf5de93fd106770b87f0911619ccd45f5b8c
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-02-25 20:46:24 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -110,7 +110,7 @@ void PipeCommand::execute() {
     }
     if (strcmp(_simpleCommands[0]->_arguments[0]->c_str(),"exit") == 0) {
       fprintf(stderr, "Good bye!!\n");
-      exit(0);
+      exit(1);
     }
     // Print contents of PipeCommand data structure
     //print();
