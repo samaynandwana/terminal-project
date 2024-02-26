bb562abe6bc13c27de00a5490869a51f2218cf27
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-02-26 15:45:37 -0500

    Commit

--- Shell.cc
+++ Shell.cc
@@ -59,7 +59,7 @@ void yyset_in (FILE *  in_str );
 extern "C" void disp_ctrlc( int sig )
 {
   fprintf(stderr, "\n");
-  Shell::TheShell->prompt();
+  //Shell::TheShell->prompt();
 }
 extern "C" void disp_zombie( int sig) {
   //wait3(0,0,NULL);
