b84e48de6a3bd6bbfec278f99a247183f4278484
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-03-02 23:24:44 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -243,7 +243,7 @@ void PipeCommand::execute() {
               std::string tok;
               fprintf(stderr, envv.c_str());
               fprintf(stderr, env_val);
-              if (!strcmp(envv, "SHELL")) {
+              if (!strcmp(envv.c_str(), "SHELL")) {
                 fprintf(stderr, "REACHED");
                 char *path = realpath("../lab3-src/shell", NULL);
                 arg.replace(start_pos, end_pos - start_pos + 1, path);
