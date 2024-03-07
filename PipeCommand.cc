18dc16016a9267e7a6de67230341ca4a765319de
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-03-06 21:46:30 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -273,7 +273,7 @@ void PipeCommand::execute() {
           int index = 0;
           char c;
           std::vector<char> buffer;
-          while (read(fdpipeout[0], &c, 1) > 0) {
+          while (read(pout[0], &c, 1) > 0) {
             if (c == '\n') {
               buffer.push_back(' ');
             } else {
