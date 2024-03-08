5e9c3ef1b96dd6afd47bbc7a61fb6fcd3494d727
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-03-07 21:39:14 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -293,7 +293,8 @@ void PipeCommand::execute() {
             //fprintf(stderr, "First: %ld\n", _simpleCommands[i]->_arguments.size());
             _simpleCommands[i]->_arguments[k] = new std::string(words[0]);
             for (int a = 1; a < words.size(); a++) {
-               _simpleCommands[i]->insertArgument(new std::string(words[a]));
+                if (words[a] != '') {
+               _simpleCommands[i]->insertArgument(new std::string(words[a]));}
                //_simpleCommands[i]->_arguments.insert(std::to_string(words[a]));
 
             }
