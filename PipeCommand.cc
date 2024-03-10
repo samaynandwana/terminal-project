d1131a6947d9cf5788d45f95b9846ee0113a99d7
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-03-09 21:49:44 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -283,22 +283,7 @@ void PipeCommand::execute() {
         args[j] = _simpleCommands[i]->_arguments[j]->c_str();
       }
       args[_simpleCommands[i]->_arguments.size()] = NULL;
-
-
-      ret = fork();
-      if (ret == 0) {
-        std::vector<char *> env_arg;
-        if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "printenv")) {
-          //print env code
-          char **p = environ;
-          while (*p != NULL) {
-            printf("%s\n", *p);
-            p++;
-          }
-          exit(0);
-        }
-        //Environment Variable Expansion
-        for (unsigned long j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
+for (unsigned long j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
           std::string& arg = *_simpleCommands[i]->_arguments[j];
           std::size_t start_pos = arg.find("${");
           while (start_pos != std::string::npos) {
@@ -335,6 +320,21 @@ void PipeCommand::execute() {
           }
         }
 
+
+      ret = fork();
+      if (ret == 0) {
+        std::vector<char *> env_arg;
+        if (!strcmp(_simpleCommands[i]->_arguments[0]->c_str(), "printenv")) {
+          //print env code
+          char **p = environ;
+          while (*p != NULL) {
+            printf("%s\n", *p);
+            p++;
+          }
+          exit(0);
+        }
+        //Environment Variable Expansion
+        
         //Tilde Expansion
         for (unsigned long j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
           //std::string& arg = *_simpleCommands[i]->_arguments[j];
