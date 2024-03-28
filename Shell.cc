5e39b7b56de38c92dc1594b8d684c67cb1e1a10c
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-03-28 12:21:33 -0400

    Commit

--- Shell.cc
+++ Shell.cc
@@ -22,6 +22,7 @@ Shell::Shell() {
     this->glob = "NULL";
     this->return_last_exit = 0;
     this->pid_background = 0;
+    this->stack = NULL;
     if ( !isatty(0)) {
 	this->_enablePrompt = false;
     }
