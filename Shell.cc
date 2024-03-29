54516ec7f746f4fdc405e77c25fd75190658171d
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-03-29 10:32:03 -0400

    Commit

--- Shell.cc
+++ Shell.cc
@@ -25,6 +25,7 @@ Shell::Shell() {
     this->pid_background = 0;
     this->ifCommandStack = *(new std::stack<IfCommand*>());
     this->listCommandStack = *(new std::stack<ListCommands*>());
+    this->listCommandStack.push(new ListCommands());
     if ( !isatty(0)) {
 	this->_enablePrompt = false;
     }
