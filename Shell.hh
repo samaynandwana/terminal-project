111f7c27d75d552822d864e0c9df0ea5024ea921
Samay Anupam Nandwana <snandwa@borg01.cs.purdue.edu>
2024-04-01 22:10:18 -0400

    Commit

--- Shell.hh
+++ Shell.hh
@@ -22,8 +22,8 @@ public:
   std::string glob;
   int return_last_exit;
   int pid_background;
-  std::stack<IfCommand*> ifCommandStack;
-  std::stack<ListCommands*> listCommandStack;
+  extern std::stack<IfCommand*> ifCommandStack;
+  extern std::stack<ListCommands*> listCommandStack;
 
   Shell();
   void execute();
