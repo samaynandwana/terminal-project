f3e06c678d51bbf1ac22ca9bc141189f20d4da73
Samay Anupam Nandwana <snandwa@data.cs.purdue.edu>
2024-04-02 23:17:08 -0400

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -415,7 +415,7 @@ void PipeCommand::execute() {
                 args[j] = (std::to_string(Shell::TheShell->pid_background)).c_str();
               } else if (!strcmp(envv.c_str(), "?")) {
                 args[j] = (std::to_string(Shell::TheShell->return_last_exit)).c_str();
-              } else if (!strcmp(envv.c_str(), "#") {
+              } else if (!strcmp(envv.c_str(), "#")) {
                 args[j] = (std::to_string(Shell::TheShell->num_args)).c_str();
               } else {
                 //base case for expansion
